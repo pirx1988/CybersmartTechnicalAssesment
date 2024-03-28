632ebcf5087ecdc033da45c87c65842a09cbd9c8
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-03-28 23:16:45 +0100

    Add migration related to Task model. Implement temples and logic for Crud operations: Add, Edit, Delete.

--- baseApp/models.py
+++ baseApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Task(models.Model):
+    # delete all tasks once user deleted, bland=True - will be changed in production system
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True) 
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        # order data by completed
+        ordering = ['completed'] 
