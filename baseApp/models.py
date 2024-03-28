61b7e359d383df111f8df04009a76928639ecc13
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-03-28 23:51:39 +0100

    Add location charField in Task model. Generate new migration related to that.

--- baseApp/models.py
+++ baseApp/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class LocationChoices(models.TextChoices):
+    LONDON = 'london', 'London'
+    BERLIN = 'paris', 'Berlin'
+    PARIS = 'wroclaw', 'Wroclaw'
+
 class Task(models.Model):
     # delete all tasks once user deleted, bland=True - will be changed in production system
     user = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True) 
@@ -9,6 +14,7 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=20, choices = LocationChoices.choices, null=True)
 
     def __str__(self):
         return self.title
