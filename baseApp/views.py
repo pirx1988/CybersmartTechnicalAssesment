22efd173dcffe0421b5f49c9f8a9cf8dc6369613
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-04-03 10:22:03 +0200

    Add communication with weatherMap Api in task list. Fetch and calculate appriopriate color for each task's location on the list. Add fallback scenarion with setting default background color in case api dosen't respond

--- baseApp/views.py
+++ baseApp/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.http import HttpResponseServerError
+import requests
+
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from .models import Task
+from weatherMapApi.utils import fetchWeather, calcBackgroundColor
+
+DEFAULT_BACKGROUND_COLOR = '#FFFFFF'  # Example default color (white)
+
 
 class CustomLoginView(LoginView):
     template_name = 'baseApp/login.html'
@@ -18,15 +22,33 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+    template_name = 'baseApp/task_list.html'
 
     def get_queryset(self):
         # Get the current logged-in user
         user = self.request.user
         # Filter tasks by the current user (sql query with WHERE statement is executed under the hood)
         queryset = Task.objects.filter(user=user)
+        for task in queryset:
+            try:
+                response = fetchWeather(task.location)
+                json_response = response.json()
+                weather = json_response['weather'][0]['main']
+                # Temperature in celsius degress of selected location
+                temp = json_response['main']['temp']
+                task.background_color = calcBackgroundColor(weather, temp)
+            except requests.HTTPError as e:
+                # Handle HTTPError (response status code is not 200)
+                # provide a default value for background color
+                task.background_color = DEFAULT_BACKGROUND_COLOR
+            except Exception as e:
+                # Handle other exceptions (e.g., network errors)
+                # provide a default value for background color
+                task.background_color = DEFAULT_BACKGROUND_COLOR
         return queryset
 
 class TaskDetail(DetailView):
@@ -34,26 +56,29 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'baseApp/task.html'
 
+
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     # field = ['title', 'description']
-    fields = ['title', 'description','location', 'completed']
+    fields = ['title', 'description', 'location', 'completed']
     # Redirect to tasks list when task created correctly
-    success_url = reverse_lazy('tasks') 
-    
-    # Method is called when the form is valid, and it's where you can customize what happens when a new task is created 
+    success_url = reverse_lazy('tasks')
+
+    # Method is called when the form is valid, and it's where you can customize what happens when a new task is created
     def form_valid(self, form):
         # Set the user of the task to the currently logged-in user
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['title', 'description','location', 'completed']
+    fields = ['title', 'description', 'location', 'completed']
     success_url = reverse_lazy('tasks')
+
 
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     # Custom object name in template
-    context_object_name = 'task' 
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
